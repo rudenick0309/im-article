64c0dae49a89053a625ee104b51cf7ddfb795770
rudenick0309 <rudenick0309@gmail.com>
2020-04-28 13:13:56 +0900

    readLineFromSoureList ing

--- src/server/helpers/fetch.js
+++ src/server/helpers/fetch.js
@@ -1,9 +1,10 @@
-const https = require('https');
+const https = require("https");
 
 async function retrieveArticle(url) {
   // TODO: retrieve the html string from given url and return as promise
+  return fetch(url, { method: "GET" });
 }
 
 module.exports = {
-  retrieveArticle
+  retrieveArticle,
 };
--- src/server/helpers/file.js
+++ src/server/helpers/file.js
@@ -3,12 +3,27 @@ const fs = require("fs");
 async function writeFile(filename, body) {
   return new Promise((resolve, reject) => {
     // TODO: 특정 파일이름(filename)을 가진 텍스트를 저장할 수 있도록 구현하세요.
+    try {
+      resolve(fs.writeFile(filename, body));
+    } catch (err) {
+      reject(err);
+    }
   });
 }
 
+// fs.readFile('/etc/passwd', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
 async function readFile(filename) {
   return new Promise((resolve, reject) => {
     // TODO: 특정 파일이름(filename)을 가진 텍스트를 읽을 수 있도록 구현하세요.
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
   });
 }
 
@@ -23,6 +38,8 @@ async function writeSourceListFile(body) {
 async function readLineFromSourceList(nthline) {
   return new Promise((resolve, reject) => {
     // TODO : ./data/source.txt에 저장되어 있는 텍스트에서 특정 줄에 해당하는 텍스트를 읽을 수 있도록 구현하세요.
+    let test = await readSourceListFile();
+    test.then(data => data.split("\n")).then(d => d[nthline])
   });
 }
 
@@ -31,5 +48,5 @@ module.exports = {
   writeSourceListFile,
   writeFile,
   readFile,
-  readLineFromSourceList
+  readLineFromSourceList,
 };
